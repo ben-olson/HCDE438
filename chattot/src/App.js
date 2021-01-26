a437b1a55756faad1349c4289d9339f3e026f687
Benjamin Olson <benjaminolson@Benjamins-MacBook-Pro.local>
2021-01-26 12:58:12 -0800

    Created Chattot React App

---
+++ chattot/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="logo"></div>
+        Chattot
+      </header>
+    </div>
+  );
+}
+
+export default App;
