c071cce8087cee8d4898177b7466841471e80b3a
Benjamin Olson <benjaminolson@Benjamins-MacBook-Pro.local>
2021-02-10 09:52:54 -0800

    Added Fireside Server

--- chattot/src/App.js
+++ chattot/src/App.js
@@ -4,9 +4,10 @@ import './scripts/TextInput';
 import TextInput from './scripts/TextInput';
 import Message from './scripts/Message';
 import UsernameInput from './scripts/NamePicker';
+import {db, useDB} from './scripts/db';
 
 function App() {
-  const [messages, setMessages] = useState([{text:'hello', username:''}])
+  const messages = useDB()
   const [username, setUsername] = useState(localStorage.getItem('username') || '')
   return (
     <div className="App">
@@ -21,13 +22,14 @@ function App() {
       <main className="messages">
 
         {messages.map((m,i)=> {
-          return <Message key={i} {...m} />
+          const isMe = m.name === username
+          return <Message key={i} {...m} isMe={isMe} />
         })}
 
       </main>
       <TextInput
         placeholder = "Write your message..."
-        send = {t=> setMessages( [{text:t, username:username, date:new Date()}, ...messages] )}
+        send = {t=> db.send({text:t, name:username, date:new Date()})}
       />
     </div>
   );
--- chattot/src/scripts/Message.js
+++ chattot/src/scripts/Message.js
@@ -1,10 +1,14 @@
 function Message(props) {
   console.log(props.date)
   return (
-    <div className="[ message ]">
-      <div className="[ message__username bold ]">{props.username}</div>
-      <div className="[ message__text ]">{props.text}</div>
-      <div className="[ message__date ]"></div>
+    <div className="[ message__container ]"
+    style={{flexDirection:props.isMe ? 'row' : 'row-reverse'}}
+    >
+      <div className="[ message ]">
+        <div className="[ message__username bold ]">{props.name}</div>
+        <div className="[ message__text ]">{props.text}</div>
+        <div className="[ message__date ]"></div>
+      </div>
     </div>
   )
 }
